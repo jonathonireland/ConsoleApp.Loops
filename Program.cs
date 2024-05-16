07cc45a7367215870cf1cc3ea1aed65ef15ed0e3
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-16 02:47:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Simple For Loop Demo
+// Print Hello World 10 Times
+
+// Global variable / Global scope
+
+Console.WriteLine($"How many times do you want Hello World! repeated?");
+int count = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Hello, World! - {i}");
+}
+Console.WriteLine($"Loop Completed");
+// Ask User how many times they wish to print hello world and print accordingly.
